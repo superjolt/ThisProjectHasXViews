79f294349196854d25804613b3038ee3ed22f936
goo goo ga ga <115384734+superjolt@users.noreply.github.com>
2025-07-03 18:26:43 +0530

    Added comments, studios, and moderation status

--- main.py
+++ main.py
@@ -13,31 +13,23 @@ if SESSION_ID is None:
 session = sa.login_by_id(SESSION_ID)
 project = session.connect_project("1193782687")
 
-# def create_thumbnail(views, loves, faves):
-#     img = Image.new('RGB', (480, 360), color='white')
-#     d = ImageDraw.Draw(img)
-    
-#     font = ImageFont.truetype("/Library/Fonts/Arial Unicode.ttf", 40)
-
-#     text = f"Views: {views}\nLoves: {loves}\nFaves: {faves}"
-#     d.text((10,10), text, fill=(0,0,0), font=font)
-
-#     img.save("thumbnail.png")
-
 views = project.views
 loves = project.loves
 faves = project.favorites
 remix = project.remix_count
 comments = len(project.comments())
+studios = len(project.studios())
+moderation = project.moderation_status()
 
 title = f"This Project Has {views} Views, {loves} Loves and {faves} Faves"
-instructions = f"And this has {remix} remixes.\nAnd {comments} comments."
+instructions = f"And this has {remix} remixes. And {comments} comments. And in {studios} studios. Moderation status: {moderation}"
 
-if not project.title == title and not project.instructions == instructions:
+print(f"Script ran. Current project stats: {views} views, {loves} loves, {faves} faves, {remix} remixes, {comments} comments.")
+
+if not project.title == title:
     project.set_title(title)
-    project.set_instructions(instructions)
-    
-    # create_thumbnail(views, loves, faves)
-    # project.set_thumbnail(file="thumbnail.png")
+    print("UPDATED PROJECT: Title")
 
-    print(f"Updated, {views} views, {loves} loves, {faves} faves, and {remix} remixes.")
+if not project.instructions == instructions:
+    project.set_instructions(instructions)
+    print("UPDATED PROJECT: Instructions")
